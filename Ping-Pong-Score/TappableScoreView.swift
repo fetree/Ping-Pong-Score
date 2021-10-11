27582c8c9f6cec21c88993e2dd8e082943a0892b
FeTree <davidjeisenbaum@gmail.com>
2021-10-10 23:21:18 -0700

    Can add scores n stuff

---
+++ Ping-Pong-Score/TappableScoreView.swift
@@ -0,0 +1,44 @@
+//
+//  TappableScoreView.swift
+//  Ping-Pong-Score
+//
+//  Created by David Eisenbaum on 10/10/21.
+//
+
+import UIKit
+
+class TappableScoreView: UIView {
+    
+    var scoreLabel = UILabel()
+    
+    init(frame: CGRect = .zero, color: UIColor) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = color
+        
+        scoreLabel.text = "0"
+        addSubview(scoreLabel)
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        scoreLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        // Make tappable
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(self.increaseScore))
+        self.addGestureRecognizer(gesture)
+    }
+    
+    @objc func increaseScore(_ sender: UITapGestureRecognizer){
+        guard let score = scoreLabel.text, var val = Int(score) else {
+            return
+        }
+        val = val + 1
+        print(val)
+        
+        scoreLabel.text = String(val)
+        // increase score
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Ping-Pong-Score/ViewController.swift
+++ Ping-Pong-Score/ViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let leftScoreView = TappableScoreView(color: .red)
+    let rightScoreView = TappableScoreView(color: .green)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUpScoreViews()
+    }
+    
+    private func setUpScoreViews() {
+        view.addSubview(leftScoreView)
+        leftScoreView.translatesAutoresizingMaskIntoConstraints = false
+        leftScoreView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        leftScoreView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5).isActive = true
+        leftScoreView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        leftScoreView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        
+        view.addSubview(rightScoreView)
+        rightScoreView.translatesAutoresizingMaskIntoConstraints = false
+        rightScoreView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        rightScoreView.leadingAnchor.constraint(equalTo: leftScoreView.trailingAnchor).isActive = true
+        rightScoreView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        rightScoreView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        
     }
 
 
